1db356e0b69e2d513dd5c715207a1ce82e9c9b36
MysteryPizzaGuy <WolfbladeM@gmail.com>
2018-04-12 22:53:21 +0200

    Initial Commit

---
+++ 10012-Data Structures And Algorithms/#6/Tasks/Task 6/Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <string>
+
+
+int main() {
+	std::string expression;
+	std::stack<int> s;
+	getline(std::cin, expression);
+	int temp;
+	int res;
+	for (size_t i = 0; i < expression.size(); i++)
+	{
+		switch (expression[i])
+		{
+		case '-':
+			temp = s.top();
+			s.pop();
+			res =s.top()- temp;
+			s.pop();
+			s.push(res);
+			break;
+		case '+':
+			temp = s.top();
+			s.pop();
+			res = s.top() + temp;
+			s.pop();
+			s.push(res);
+			break;
+		case'/':
+			temp = s.top();
+			s.pop();
+			res = s.top() / temp;
+			s.pop();
+			s.push(res);
+			break;
+		case'*':
+			temp = s.top();
+			s.pop();
+			res = s.top() * temp;
+			s.pop();
+			s.push(res);
+			break;
+		case ' ':
+			break;
+		default:
+			s.push(expression[i] - '0');
+			break;
+		}
+		
+	}
+
+	if (!s.empty())
+	{
+		std::cout<<s.top();
+	}
+
+
+	std::cout << std::endl;
+	return 0;
+}
